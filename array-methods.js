4a6da6ad1eeb73909fb821324a7822ff1f28da3a
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-08 21:21:16 +1000

    array methods

---
+++ array-methods.js
@@ -0,0 +1,21 @@
+//!array methods
+let numbers = [23, 45, 67, 4, 5, 6];
+//push(1,23) = > length
+//unshift()
+//pop() => last element
+//shift() => first element
+//includes() = >boolean
+//indexOf() => index / -1
+
+//hof
+//takes call back function as an input
+
+//loop
+//array.forEach(callback)
+const callback = (value, index, array) => {
+  console.log(value, index, array);
+};
+// numbers.forEach(callback);
+numbers.forEach((v, index) => {
+  console.log(a);
+});
