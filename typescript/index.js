a5180024f933f15c446f250dd234c562a574ad6a
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-22 21:10:53 +1000

    typescript basics

---
+++ typescript/index.js
@@ -0,0 +1,24 @@
+"use strict";
+//typescript
+//data types
+//string
+//number
+//boolean
+//null
+//undefined
+//any
+//unknown
+//variable declaration
+//let, const, var
+// let str = "hello";
+// console.log(str);
+// //str=23;
+// let num;
+// num = 12;
+// //num = "hello";
+// num = 45;
+// num = 45.34;
+// let isLoggedIn = true;
+// isLoggedIn = false;
+// let a = null;
+// let b;
--- typescript/index.ts
+++ typescript/index.ts
@@ -11,4 +11,32 @@
 
 //variable declaration
 //let, const, var
-let str: string = "hello";
+// let str: string = "hello";
+// console.log(str);
+// //str=23;
+
+// let num: number;
+// num = 12;
+// //num = "hello";
+// num = 45;
+// num = 45.34;
+// let isLoggedIn: boolean = true;
+// isLoggedIn = false;
+// let a: null = null;
+
+// let b: undefined = undefined;
+
+// //!any
+// let c: any;
+// c = 5;
+// c = "any type";
+// console.log(c.toUpperCase());
+
+// //!unknown
+// let d: unknown;
+// d = 34;
+// d = "unknown type";
+
+// if (typeof d === "string") {
+//   console.log(c.toUpperCase());
+// }
