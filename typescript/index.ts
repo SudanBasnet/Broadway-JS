97204b6f50a85dc36e1b5fce0b74dc254488ebff
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-23 22:33:42 +1000

    other topics

--- typescript/index.ts
+++ typescript/index.ts
@@ -151,9 +151,9 @@ let user_acc: Ireg = {
 
 //function
 
-const add = (a: number, b: number): number => {
-  return a + b;
-};
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
 
 //void
 
@@ -165,3 +165,81 @@ let greet = (name: string): void => {
 const never = (): never => {
   throw "new error";
 };
+
+const add1 = (...numbers: number[]) => {
+  //return a+b;
+};
+
+//!union [|],
+
+let id: number | string = 12;
+id = "2";
+//id = true;
+
+//type [type alias]
+type A = {
+  password: string;
+  email: string;
+};
+type addF = (x: number, y: number) => number;
+const add: addF = (a, b) => {
+  return a + b;
+};
+//intersection[&]
+type B = {
+  name: string;
+};
+
+type C = A & B;
+type D = A | B;
+let c: C = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+let d: D = {
+  email: "",
+  password: "",
+};
+
+//diff between type and interfce
+
+interface E {
+  e: string;
+}
+
+interface E {
+  f: string;
+}
+
+let e: E = {
+  e: "",
+  f: "",
+};
+
+// type literal
+
+let res_status: "success" | "fail" | "error";
+type STATUS = "success" | "fail" | "error";
+
+let Status: STATUS;
+res_status = "success";
+res_status = "error";
+
+
+//class
+class Person {
+  name:string;
+  protected email: string;
+  private password: string;
+  public age:number;
+}
+
+constructor (name:string,email: string, password: string, age:number){
+  this.name = name;
+  this.age = age;
+  this.email = email;
+  this.password = pass;
+
+}
