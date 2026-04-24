a7dcb9d8d2f1972fa1db08ed7ac601f6f4d9565a
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-24 21:59:01 +1000

    github basics

---
+++ git.js
@@ -0,0 +1,8 @@
+//git and github/gitlab/bitbucket
+//git =>vcs
+
+//commands
+//git init => initialize empty repo at folder
+//git config --global --list => list local git global config
+//git config --global --list user.email "abc@gmail.com"
+//WD => staging area
