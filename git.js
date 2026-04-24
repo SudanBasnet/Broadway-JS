3243f68bb40b15890cbd5b645a622e93148aa8c9
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-24 22:15:11 +1000

    testing the branching

--- git.js
+++ git.js
@@ -6,7 +6,12 @@
 //git config --global --list => list local git global config
 //git config --global --list user.email "abc@gmail.com"
 //WD => staging area => local repo commit
+//git status
 // git add <file_path> => wd -> SA
 //git commit -m "<commit Message"  => SA -> local branch/repo
+//git add . => all changes to add
 
-//ok
+//branches
+
+//git branch => list all local branches
+//git branch <branch_name> => create new branch
