97b5f8ca7368f98a26376ea86ffcbd2e23f57886
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-25 23:14:41 +1000

    serbver creation

--- node.js
+++ node.js
@@ -13,3 +13,46 @@ app.listen(3000, () => {
 // npm init -y        # create package.json
 // npm install pkg    # install package
 // npm start          # run script (if defined)
+
+//creating the server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// sample data
+let users = [
+  { id: 1, name: "Sudan" },
+  { id: 2, name: "John" },
+];
+
+// GET all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// GET single user
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id == req.params.id);
+  res.json(user || { message: "User not found" });
+});
+
+// POST new user
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+  };
+  users.push(newUser);
+  res.json(newUser);
+});
+
+// DELETE user
+app.delete("/users/:id", (req, res) => {
+  users = users.filter((u) => u.id != req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
